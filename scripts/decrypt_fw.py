b8d95e1201b75f41d3d2777f95247b13811e65b0
Kyle Machulis <kyle@nonpolynomial.com>
2016-04-24 00:48:48 -0700

    OMFG STOP OVERWRITING THE DECRYPTION WHAT IS WRONG WITH YOU

--- scripts/decrypt_fw.py
+++ scripts/decrypt_fw.py
@@ -1,23 +1,21 @@
-with open("312-16-decrypted.bin") as f:
+with open("312-16.upg") as f:
     b = f.read(15872)
 
 iv = [0xb9, 0xfe, 0x8f]
 keys = [0x65, 0xed, 0x83]
-g = open("312-16-encrypted.bin", "wb")
+g = open("d16.bin", "wb")
 for i in range(0,len(b)):
     if i % 3 == 0:
-        # TODO: Make this actually like, you know, work.
         n = ord(b[i])
         n2 = (((n^0x62) + 0x41) & 0xff)
-        iv[0] = n2 ^ iv[0] ^ keys[0]
-        g.write(chr(iv[0]))
+        g.write(chr(n2 ^ iv[0] ^ keys[0]))
+        iv[0] = ord(b[i])
     if i % 3 == 1:
-        n = ord(b[i])
-        # calculate
-        iv[1] = n ^ iv[1] ^ keys[1]
         # swap nibbles of input
-        iv[1] = ((iv[1] >> 4) | ((iv[1] & 0x0f) << 4))
-        g.write(chr(iv[1]))
+        n = ord(b[i])
+        n2 = (n >> 4) | ((n & 0x0f) << 4)
+        g.write(chr(n2 ^ iv[1] ^ keys[1]))
+        iv[1] = ord(b[i])
     if i % 3 == 2:
-        iv[2] = ord(b[i]) ^ keys[2] ^ iv[2]
-        g.write(chr(iv[2]))
+        g.write(chr(ord(b[i]) ^ keys[2] ^ iv[2]))
+        iv[2] = ord(b[i])
