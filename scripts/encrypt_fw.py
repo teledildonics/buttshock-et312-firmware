f52647b27b4138442f5cbea2202d071ad6649efe
Kyle Machulis <kyle@nonpolynomial.com>
2016-04-24 13:30:38 -0700

    Fixed first byte encryption

--- scripts/encrypt_fw.py
+++ scripts/encrypt_fw.py
@@ -1,15 +1,17 @@
-with open("312-16-decrypted.bin") as f:
+with open("312-16-decrypted-changed.bin") as f:
     b = f.read(15872)
 
 iv = [0xb9, 0xfe, 0x8f]
 keys = [0x65, 0xed, 0x83]
-g = open("312-16-encrypted.bin", "wb")
+g = open("312-16-encrypted-changed.bin", "wb")
 for i in range(0,len(b)):
     if i % 3 == 0:
-        # TODO: Make this actually like, you know, work.
-        n = ord(b[i])
-        n2 = (((n^0x62) + 0x41) & 0xff)
-        iv[0] = n2 ^ iv[0] ^ keys[0]
+        n = ord(b[i]) ^ iv[0] ^ keys[0]
+        n -= 0x41
+        if n < 0:
+            n += 0x100
+        n &= 0xff
+        iv[0] = n ^ 0x62
         g.write(chr(iv[0]))
     if i % 3 == 1:
         n = ord(b[i])
