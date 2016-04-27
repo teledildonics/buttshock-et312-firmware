212dd1a26d049c718c40a1825eaa8a9e47ea314c
Kyle Machulis <kyle@nonpolynomial.com>
2016-04-26 23:10:33 -0700

    Clean up old scripts now that we have fw-util

--- scripts/crc.py
+++
@@ -1,9 +0,0 @@
-with open("312-16-decrypted-changed.bin") as f:
-    b = f.read()
-xor = 0
-add = 0
-for c in range(len(b) - 16):
-    xor ^= ord(b[c])
-    add += ord(b[c])
-print "0x%.02x 0x%.02x 0x%.02x" % (xor, (add & 0xff), ((add >> 8) & 0xff))
-
