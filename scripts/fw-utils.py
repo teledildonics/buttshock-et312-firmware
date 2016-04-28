eb0b44ff89adeb25d812c2293b8806b7f3d9b34d
Kyle Machulis <kyle@nonpolynomial.com>
2016-04-27 22:02:00 -0700

    Added uploader and space filler to utility. Fixes #7

--- scripts/fw-utils.py
+++ scripts/fw-utils.py
@@ -12,6 +12,7 @@ class ET312FirmwareUtils(object):
         self.iv = copy.copy(ET312FirmwareUtils.IV)
         with open(input_file, "rb") as f:
             self.input_file = bytearray(f.read())
+        self.fill_space()
         if output_file:
             self.output_file = open(output_file, "wb")
 
@@ -24,7 +25,8 @@ class ET312FirmwareUtils(object):
         return [xor, (add & 0xff), ((add >> 8) & 0xff)]
 
     def encrypt(self, write_crc=True):
-        funcs = {0: lambda x: ((x - 0x41 if x >= 0x41 else ((x - 0x41) + 0x100)) ^ 0x62) & 0xff,
+        funcs = {0: lambda x: ((x - 0x41 if x >= 0x41
+                                else ((x - 0x41) + 0x100)) ^ 0x62) & 0xff,
                  1: lambda x: (x >> 4) | ((x & 0x0f) << 4),
                  2: lambda x: x}
 
@@ -52,8 +54,30 @@ class ET312FirmwareUtils(object):
             self.output_file.write(bytearray([output]))
             self.iv[choice] = n
 
-    def upload():
-        pass
+    def fill_space(self):
+        self.input_file += bytearray([0] * (15872 - len(self.input_file)))
+
+    def upload(self, port):
+        import serial
+        from xmodem import XMODEM
+        import io
+        # In case xmodem starts acting up
+        # import logging
+        # logging.basicConfig(level=logging.DEBUG)
+
+        s = serial.Serial(port, 19200, timeout=1,
+                          parity=serial.PARITY_NONE,
+                          bytesize=8, stopbits=1,
+                          xonxoff=0, rtscts=0)
+
+        def getc(size, timeout=1):
+            return s.read(size)
+
+        def putc(data, timeout=1):
+            return s.write(data)
+
+        modem = XMODEM(getc, putc)
+        modem.send(io.BytesIO(self.input_file))
 
 
 def main():
@@ -67,19 +91,20 @@ def main():
     parser.add_argument("-e", "--encrypt", dest="encrypt", action="store_true",
                         help="Encrypt input file, store in output file."
                         " Adds checksum to output by default.")
-    parser.add_argument("-u", "--upload", dest="upload", action="store_true",
-                        help="Upload input file to box (requires serial and xmodem packages)")
+    parser.add_argument("-u", "--upload", dest="upload",
+                        help="Upload input file to box, expects com port as argument."
+                        "(requires serial and xmodem packages)")
     parser.add_argument("-c", "--crc", dest="crc", action="store_true",
                         help="Output xor/checksum for input file")
     args = parser.parse_args()
 
     if not args.input_file:
-        print("ERROR: ET-312 Firmware Utility requires an input file to run.")
+        print("ERROR: Input file required to run.")
         parser.print_help()
         return 1
 
     if (args.decrypt or args.encrypt) and not args.output_file:
-        print("ERROR: ET-312 Firmware Utility requires an output file for encryption/decryption.")
+        print("ERROR: Output file required for encryption/decryption.")
         parser.print_help()
         return 1
 
@@ -88,6 +113,11 @@ def main():
         etfw.encrypt()
     elif args.decrypt:
         etfw.decrypt()
+    elif args.upload:
+        etfw.upload(args.upload)
+    elif args.crc:
+        print(etfw.generate_crc())
+    return 0
 
 if __name__ == "__main__":
     main()
